0260328672c6dcacc3bb702dd8227b0f9959674f
savagewil <wxs5474@rit.edu>
2019-05-12 10:17:48 -0400

    Built Full multiplier

--- multiplier_1.sv
+++ multiplier_1.sv
@@ -1,15 +1,15 @@
 module multiplier_1(
     input wire [31:0] in,
     input wire [31:0] constant,
-    output wire [31:0] out);
-    wire [63:0] mulTemp;
-    wire [31:0] choiceUnsigned;
+    output reg [31:0] out);
 
     always_comb begin
+    	reg [63:0] mulTemp;
+    	reg [31:0] choiceUnsigned;
         choiceUnsigned = constant;
         if (constant[31]) choiceUnsigned = (~constant) + 1;
         mulTemp = in * choiceUnsigned;
-        out = mulTemp / 1'hFFFFFFFF;
+        out = mulTemp / 32'hFFFFFFFE;
         if (constant[31]) out = (~out) + 1;
     end
 endmodule : multiplier_1
--- testbed.sv
+++ testbed.sv
@@ -2,24 +2,18 @@
 `timescale 1ns/1ns
 module testing_tb();
 	reg clk;
-	reg [1:0] count;
-	reg [3:0] [31:0] choices;
+	reg [31:0] count;
 	wire [31:0] f;
-	
 
 	initial begin
 		clk = 1;
-		count = -1;
-		choices[0] = 0;
-		choices[1] = 1;
-		choices[2] = 2;
-		choices[3] = 3;
+		count = -5;
+
 	end
 	always #5 clk = ~clk;
 	always @(posedge clk)
 		count = count + 1;
-	
-	multiplier_1 MUT(count, choices, f);
+
 endmodule
 
 		
